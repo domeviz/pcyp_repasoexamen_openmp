99fa0396d4fbdf26e885eda5c521b0d563097720
Domenica Vizcarra <domeviz3410@gmail.com>
2024-02-26 16:39:29 -0500

    Librerias

--- main.cpp
+++ main.cpp
@@ -1,4 +1,10 @@
 #include <iostream>
+#include <omp.h>
+#include <vector>
+#include <fmt/core.h>
+#include <fmt/ranges.h>
+#include <fstream>
+#include <chrono>
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
