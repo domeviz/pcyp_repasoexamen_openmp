4baec6957bb6532327dcddb97e38608f1bcfaea6
Domenica Vizcarra <domeviz3410@gmail.com>
2024-02-26 16:51:01 -0500

    Leer datos

--- main.cpp
+++ main.cpp
@@ -6,7 +6,24 @@
 #include <fstream>
 #include <chrono>
 
+std::vector<int> read_file() {
+    std::fstream fs("../datos.txt", std::ios::in );
+    std::string line; std::vector<int> ret;
+    while( std::getline(fs, line) ){
+        ret.push_back( std::stoi(line) );
+    }
+    fs.close();
+    return ret;
+}
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
+
+    std::vector<int> datos=read_file();
+    for(int i =0; i<10;i++){
+        fmt::println("{}",datos[i]);
+    }
+
+
     return 0;
 }
